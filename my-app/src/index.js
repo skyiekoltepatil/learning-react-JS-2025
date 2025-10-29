33250ea1ed59bbf2b7d8bf112d244a919e71f199
skyiekoltepatil <bhushankolte20@gmail.com>
2025-10-30 00:46:23 +0530

    session 3 lecture 31
    props dynamics setups

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,15 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const author = 'Jordan Moore';
-const title = 'Interesting Facts For Curious Minds';
-const img ='/book1.jpg'
-
+const firstBook = {
+   author: 'Jordan Moore',
+   title: 'Interesting Facts For Curious Minds',
+   img : '/book1.jpg',
+}
+const secondBook = {
+  author: 'Alex Michaelides',
+  title: 'THE SILENT PATIENT',
+  img:'https://m.media-amazon.com/images/I/81JJPDNlxSL._AC_UY218_.jpg',
+}
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author = {author} title = {title} img = {img}/>
-      <Book author = {author} title = {title} img = {img}/>
+      <Book author = {firstBook.author} title = {firstBook.title} img = {firstBook.img}/>
+      <Book author = {secondBook.author} title = {secondBook.title} img = {secondBook.img}/>
 
      
     </section>
