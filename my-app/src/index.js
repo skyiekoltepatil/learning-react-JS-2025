f6d48e7e71a87f5d4191e1d8a10f05ebf0f5e5c2
skyiekoltepatil <bhushankolte20@gmail.com>
2025-10-30 00:33:01 +0530

    session 3 lecture 30
    props

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,29 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const author = 'Jordan Moore';
+const title = 'Interesting Facts For Curious Minds';
+const img ='/book1.jpg'
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book></Book>
-      <Book></Book>
-      <Book></Book>
+      <Book author = {author} title = {title} img = {img}/>
+      <Book author = {author} title = {title} img = {img}/>
+
+     
     </section>
   );
 };
-const author = 'Jordan Moore';
 
-const Book = () => {
-  const title = 'Interesting Facts For Curious Minds';
+const Book = (props) => {
+  console.log(props);
+  
   return (
     <article className='book'>
-      <img
-        src='/book1.jpg'
-        alt='Interesting Facts For Curious Minds'
-      />
-      <h2>{title}</h2>
-      <h4>{author.toUpperCase()}</h4>
-      <p>{50 + 50}</p>
-    </article>
+      <img src= {props.img} alt={props.title}/>
+       <h2>{props.title}</h2>
+       <h4>{props.author}</h4>
+      </article>
   );
 };
 
