3a1d3a9ea690495ea061b45185963cda1948f992
skyiekoltepatil <bhushankolte20@gmail.com>
2025-10-31 01:34:44 +0530

    session 6 lecture 86
    render the list
    remove items
    useState with Array

--- react-hooks/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ react-hooks/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,34 @@
+import React from 'react';
+import { data } from '../../../data';
+console.log(data)
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, People] = React.useState(data);
+
+  const magic = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    People(newPeople);
+  };
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => magic(id)}>magic</button>
+          </div>
+        );
+      })}
+      <button
+        className='btn'
+        style={{ marginTop: '2rem' }}
+        onClick={() => People([])}
+      >
+        click to see magic
+      </button>
+    </div>
+  );
 };
 
 export default UseStateArray;
