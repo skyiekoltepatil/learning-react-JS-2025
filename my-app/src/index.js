260d5d3a319d056aa8cee6e65d2db7ecac666dc1
skyiekoltepatil <bhushankolte20@gmail.com>
2025-10-30 16:38:28 +0530

    session 3 lecture 36
    alert button

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,12 +7,13 @@ const  books=[
    author: 'Jordan Moore',
    title: 'Interesting Facts For Curious Minds',
    img : '/book1.jpg',
-  
+   
 },
 {
   author: 'Alex Michaelides',
   title: 'THE SILENT PATIENT',
   img:'https://m.media-amazon.com/images/I/81JJPDNlxSL._AC_UY218_.jpg',
+  
 },
 ]
 
@@ -21,12 +22,36 @@ const  books=[
 const BookList = () => {
   return (
     <section className="booklist">
-       {books.map((book)=>{
-        const{img,title,author} = book;
-        return <Book img={img} title={title} author={author}/>
+       {/* {books.map((book)=>{
+        const{img,title,author,}= book;
+        return <Book img={img} title={title} author={author} />;
 
        })};
-        
+        */}
+        <EventExamples/>
+    </section>
+  );
+};
+const EventExamples = () => {
+  const buttonClick = () => {
+    alert('button has been click');
+  };
+ const handleFormInput= ()=>{
+  console.log('an input has been provided')
+ }
+
+return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type='text'
+          name='hello'
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+      </form>
+      <button onClick={buttonClick}>click me</button>
     </section>
   );
 };
@@ -38,6 +63,7 @@ const Book = (props) => {
       <img src= {img} alt={title}/>
        <h2>{title}</h2>
        <h4>{author}</h4>
+       
         
       </article>
       
