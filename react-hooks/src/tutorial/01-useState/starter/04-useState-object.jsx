bd1ca6d76cf244a65f32c141d372d0ca19dcf96e
skyiekoltepatil <bhushankolte20@gmail.com>
2025-10-31 03:50:58 +0530

    session6 lecture 89
    displayPerson example

--- react-hooks/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ react-hooks/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,24 +1,26 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  const [name, setName] = useState('shruti');
-  const [age, setAge] = useState(18);
-  const [talk, setTalk] = useState('hello');
-
+const [person,setPerson]= useState ({
+   name : 'shruti',
+   age : 18,
+   talk : 'hello',
+   
+});
   const displayPerson = () => {
-    setName('skyie');
-    setAge(17);
-    setTalk('HI');
-  };
+    setPerson ({name:'skyie',age:17,talk:'HI',
+      // setPerson({ name: 'fatty' });
+    // setPerson({ ...person, name: 'fatty' });
+  })};
   return (
     <>
-      <h3>{name}</h3>
-      <h3>{age}</h3>
-      <h4>says: {talk}</h4>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h4> says: {person.talk}</h4>
       <button className='btn' onClick={displayPerson}>
         show skyie
       </button>
     </>
   );
 };
-export default UseStateObject;
+export default UseStateObject
