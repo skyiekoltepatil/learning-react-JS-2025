33eab418ccaa6f407c16bf306d208bce403eab88
skyiekoltepatil <bhushankolte20@gmail.com>
2025-11-17 16:01:01 +0530

    02-useEffect-basics

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,21 +1,25 @@
-import Starter from "./tutorial/02-useEffect/starter/01-code-example";
+import Starter from "./tutorial/02-useEffect/starter/02-useEffect-basics";
 
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
-const CodeExample = () => {
+const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
   const sayHello = () => {
-    console.log('hello world');
-    
+    console.log("hello world");
   };
+
   sayHello();
+
+  useEffect(() => {
+    console.log("render");
+  }, []);
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>
   );
 };
-export default CodeExample;
+export default UseEffectBasics;
